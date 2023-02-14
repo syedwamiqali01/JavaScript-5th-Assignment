19314d29d50242b4f266a44de452c17a62c29754
syedwamiqali01 <123824011+syedwamiqali01@users.noreply.github.com>
2023-02-14 17:20:43 +0500

    Add files via upload

---
+++ javascript Assignment 5th/app.js
@@ -0,0 +1,73 @@
+// const A = [24, 53, 78, 91, 12];
+
+// let smallest = A[0]; // Assume the first number is the smallest
+
+// for (let i = 1; i < A.length; i++) {
+//   if (A[i] < smallest) {
+//     smallest = A[i];
+//   }
+// }
+
+// console.log("The smallest number in the array is:", smallest);
+
+
+// for (let i = 1; i <= 20; i++) {
+//     console.log(i * 5);
+//   }
+
+
+//   const A = [24, 53, 78, 91, 12];
+
+
+
+// let largest = A[0]; // Assume the first number is the largest
+
+// for (let i = 1; i < A.length; i++) {
+//   if (A[i] > largest) {
+//     largest = A[i];
+//   }
+// }
+
+// console.log("The largest number in the array is:", largest);
+
+
+
+
+// const A = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+// let userInput = prompt("Enter an item to search for:");
+
+// let found = false;
+
+// for (let i = 0; i < A.length; i++) {
+//   if (A[i] === userInput) {
+//     found = true;
+//     break;
+//   }
+// }
+
+// if (found) {
+//   console.log(userInput + " is found in the list!");
+// } else {
+//   console.log(userInput + " is not found in the list.");
+// }
+
+
+
+
+// 
+
+
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(i);
+//   }
+
+
+// const num = parseInt(prompt("Enter the number to generate the multiplication table: "));
+// const length = parseInt(prompt("Enter the length of the multiplication table: "));
+
+// for (let i = 1; i <= length; i++) {
+//   console.log(`${num} x ${i} = ${num*i}`);
+// }
+
